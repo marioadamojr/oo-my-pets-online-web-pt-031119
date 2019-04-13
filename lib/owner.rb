9025a674704cbf5198b822e9dfaba3548018faf0
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-13 18:33:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,12 @@
 class Owner
   # code goes here
   attr_accessor
-  attr_reader :name
+  attr_reader :name, :species
   @@all = []
 
   def initialize(name)
     @name = name
+    @species = "human"
     @@all << self
   end
 
